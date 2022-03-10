7381f910bef8fd6919970fb402b2c6cc1239f93f
rayhanyeager <86096057+rayhanyeager@users.noreply.github.com>
2022-03-10 10:36:36 +0700

    Initial commit

---
+++ src/main/java/id/ac/uin/Latihan1/demo/IndexController.java
@@ -0,0 +1,17 @@
+package id.ac.uin.Latihan1.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class IndexController {
+    @GetMapping("/hello")
+    public String index (@RequestParam String nama) {
+        return "Selamat belajar Spring Boot!" + nama;
+    }
+
+}
